bdf385ee5a129e46f7aa8ce58181394915f1c2a5
Nandyprasan <49917009+Nandyprasan@users.noreply.github.com>
2020-03-19 16:17:45 +0530

    Final UI script

--- FirstCry/src/main/java/ing/firstcry/pages/Homepage.java
+++ FirstCry/src/main/java/ing/firstcry/pages/Homepage.java
@@ -6,6 +6,9 @@ import java.util.logging.Logger;
 
 import javax.xml.xpath.XPath;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
@@ -19,39 +22,54 @@ import ing.common.utilities.CommonUtilities;
 public class Homepage {
 	StepDefinition step;
 	CommonUtilities commonUtilities=new CommonUtilities();
+	Actions action = new Actions(step.driver);
 	
+	@FindBy (id="deny")
+	private WebElement homepopup;
+	
+	@FindBy(tagName="iframe")
+	private WebElement framepopup;
 	
 	@FindBy (xpath="//li[@class='B14_42 allcat']/a[@href='/']")
 	private WebElement allcategorieslink;
 	
-	@FindBy(css="button#deny")
-	private WebElement homepopup;
+	@FindBy (xpath="//a[text()='All Categories']")
+	private WebElement insideallcategorieslink;
+	
+	
+
 	
 	@FindBy(xpath="//a[text()='STORES & PLAYSCHOOLS']")
 	private WebElement storeandplayschool;
 	
 	public void mouseoverallcateglink(String Linktoclick){
+		commonUtilities.implicitwait();
+//		step.driver.switchTo().frame(framepopup);
+//		
+//		System.out.println("switched to frame");
+//		JavascriptExecutor exe = (JavascriptExecutor) step.driver;
+//		exe.executeScript("document.getElementById('deny').click()");
+	//	homepopup.click();
+		step.driver.switchTo().defaultContent();
 		if (Linktoclick.equalsIgnoreCase("All Categories")){
 			{
-				Actions action = new Actions(step.driver);
    			action.moveToElement(allcategorieslink).perform();
+   			action.sendKeys(Keys.PAGE_DOWN);
 			}
 		}
-		Set<String> setpopup =step.driver.getWindowHandles();
-		Iterator<String> iterator=setpopup.iterator();
-		for(String id:setpopup){
-			iterator.next();
-		}
+//		step.driver.switchTo().parentFrame();
 		
 		
-		commonUtilities.implicitwait();
-		homepopup.click();
 	}	
 	
 	public void mouseoverstoreandplayschool(String Storesandplayschools){
-		if(Storesandplayschools.equalsIgnoreCase("STORES & PLAYSCHOOLS")){
-		Actions action = new Actions(step.driver);
+if(insideallcategorieslink.isDisplayed()){
+	action.moveToElement(insideallcategorieslink).perform();
+	
+	if(Storesandplayschools.equalsIgnoreCase("STORES & PLAYSCHOOLS")){
 		action.moveToElement(storeandplayschool).perform();
 		}
-	}
+
+		}
+			}
 }
